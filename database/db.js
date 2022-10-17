402967f5a9080dec6ff5c3d56cfdd413dfc2a540
bookips-Eric <bookips-sam@bookips-samui-MacBookPro.local>
2022-10-17 16:13:38 +0900

    dotenv

--- database/db.js
+++ database/db.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 function connectDB() {
-    return mongoose.connect("mongodb+srv://rkddbtls007:Asd20902!!@cluster0.obv4g.mongodb.net/?retryWrites=true&w=majority", {
+    return mongoose.connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
